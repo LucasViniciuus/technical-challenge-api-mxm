54db6870a3efec2b78a95c565cadd56bc9f6fb23
LucasViniciuus <lucas.vinicius03@outlook.com>
2024-04-22 00:44:32 -0300

    feat: new form that registers products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductRegisterComponentComponent } from './product-register-component/product-register-component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductRegisterComponentComponent],
+  imports: [RouterOutlet, ProductRegisterComponentComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/product-register-component/product-register-component.component.ts
+++ src/app/product-register-component/product-register-component.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { error } from 'console';
+import { AppComponent } from '../app.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-register-component',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './product-register-component.component.html',
   styleUrl: './product-register-component.component.css'
 })
 export class ProductRegisterComponentComponent {
+nome: string = '';
+identificacao: string = '';
+preco: number = 0;
+quantidade: number = 0;
+
+
+  constructor(private http: HttpClient){}
+
+  cadastrarProduto() {
+
+    const url = 'http://ec2-3-83-149-60.compute-1.amazonaws.com:1880/cadastroProduto';
+    const produto = {
+      nome: this.nome,
+      identificacao: this.identificacao,
+      preco: this.preco,
+      quantidade: this.quantidade
+    };
+
+    this.http.post(url, produto).subscribe(
+      response => {
+        console.log('produto cadastrado com sucesso.', response);
+      },
+      error => {
+        console.error('Erro ao cadastrar produto', error);
+      }
+    );
+  }
+
+  
 
 }
