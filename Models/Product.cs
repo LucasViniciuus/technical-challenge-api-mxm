170f91cf736f7134bcd7c0603001e6eaf9872eb2
LucasViniciuus <lucas.vinicius03@outlook.com>
2024-04-28 19:47:35 -0300

    fix: product

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class Product
     {
+        [Key]
         public int id { get; set; }
         public string name { get; set; }
         public double price { get; set; }
@@ -11,8 +14,8 @@ namespace Models
         public string supplier { get; set; }
         //ver possibilidade de criação de tabela fornecedor, pois assim caso tenha tempo, faça um cadastro de fornecedores.
         public DateTime expirationDate { get; set; } = DateTime.Now;
-        public Double unitMeasure { get; set; }
-        public Double weight { get; set; }
+        public int unitMeasure { get; set; }
+        public double weight { get; set; }
         public DateTime arrivalDate { get; set; } = DateTime.Now;
         public string team { get; set; }
     }
